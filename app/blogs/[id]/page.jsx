4f8b422f703e7e04d9354892c080dc2ded8d73a1
yashj29 <29yashjain29@gmail.com>
2025-04-18 14:16:23 +0530

    working

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,6 +1,8 @@
 'use client'
 import { assets, blog_data } from '@/Assets/assets';
+import Footer from '@/Components/Footer';
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 
 
@@ -26,14 +28,16 @@ const page = ({params}) => {
   return (data?<>
     <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
       <div className='flex justify-between items-center'>
+        <Link href='/'>
         <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
+        </Link>
         <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>
           Get started <Image src={assets.arrow} alt='' />
         </button> 
       </div>
       <div className='text-center my-24'>
         <h1 className='text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto'>{data.title}</h1>
-        <Image className='mx-auto mt-6 border border-white rounded-full' scr={data.author_img} width={60} height ={60} alt=''/>
+        <Image className='mx-auto mt-6 border border-white rounded-full' scr={data.author_img} width={60} height ={60} alt=''/>  
         <p className='mt-1 pb-2 text-lg max-w-[740px] mx-auto'>{data.author}</p>
       </div>
     </div>  
@@ -41,12 +45,33 @@ const page = ({params}) => {
       <Image className='border-4 border-white' src={data.image} width={1280} height={720} alt=''/>
       <h1 className='my-8 text-[26px] font-semibold'>Introduction:</h1>
       <p>{data.description}</p>
+      <h3 className='my-5 text-[18px] font-semibold'>Step 1: Self-Reflection and Goal Setting</h3>
+      <p className='my-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam sequi ab nisi voluptatem optio quo, tenetur cumque et ducimus numquam nihil quam aliquid fuga, dolore saepe rem quos nostrum harum!</p>
+      <p className='my-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam sequi ab nisi voluptatem optio quo, tenetur cumque et ducimus numquam nihil quam aliquid fuga, dolore saepe rem quos nostrum harum!</p>
 
+      <h3 className='my-5 text-[18px] font-semibold'>Step 2: Self-Reflection and Goal Setting</h3>
+      <p className='my-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam sequi ab nisi voluptatem optio quo, tenetur cumque et ducimus numquam nihil quam aliquid fuga, dolore saepe rem quos nostrum harum!</p>
+      <p className='my-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam sequi ab nisi voluptatem optio quo, tenetur cumque et ducimus numquam nihil quam aliquid fuga, dolore saepe rem quos nostrum harum!</p>
 
+      <h3 className='my-5 text-[18px] font-semibold'>Step 3: Self-Reflection and Goal Setting</h3>
+      <p className='my-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam sequi ab nisi voluptatem optio quo, tenetur cumque et ducimus numquam nihil quam aliquid fuga, dolore saepe rem quos nostrum harum!</p>
+      <p className='my-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam sequi ab nisi voluptatem optio quo, tenetur cumque et ducimus numquam nihil quam aliquid fuga, dolore saepe rem quos nostrum harum!</p>
 
+      <h3 className='my-5 text-[18px] font-semibold'>Conclusion:</h3>
+      <p className='my-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam sequi ab nisi voluptatem optio quo, tenetur cumque et ducimus numquam nihil quam aliquid fuga, dolore saepe rem quos nostrum harum!</p>
+      <div className='my-24'>
+        <p className='text-black font font-semibold my-4'>Share this article on social media</p>
+        <div className='flex'>
+          <Image src={assets.facebook_icon} width={50} alt=''/>
+          <Image src={assets.twitter_icon} width={50} alt=''/>
+          <Image src={assets.googleplus_icon} width={50} alt=''/>
+        </div>
+      </div>
     </div>
+    <Footer />
     </>:<></>
   )
 }
 
 export default page
+
